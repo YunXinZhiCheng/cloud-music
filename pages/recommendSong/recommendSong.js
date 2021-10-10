b5705ab279f966be6927540d230b517a9e79bf16
YunXinZhiCheng <867395636@qq.com>
2021-10-10 21:45:57 +0800

    每日推荐页-歌曲列表动态数据

--- pages/recommendSong/recommendSong.js
+++ pages/recommendSong/recommendSong.js
@@ -1,4 +1,8 @@
 // pages/recommendSong/recommendSong.js
+
+// 引入网络请求函数
+import request from '../../utils/request'
+
 Page({
 
   /**
@@ -7,16 +11,44 @@ Page({
   data: {
     day: '', // 天数
     month: '', // 月份
+    recommendList: [] // 推荐列表数据
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // 判断用户是否登录
+    let userInfo = wx.getStorageSync('userInfo')
+    if (!userInfo) {
+      wx.showToast({
+        title: '请先登录',
+        icon: 'none',
+        success: () => {
+          // 跳转到登录页面
+          wx.reLaunch({
+            url: '/pages/login/login',
+          })
+        }
+      })
+    }
+    // 获取每日推荐的数据
+    this.getRecommendList()
+
     // 更新日期的状态数据
     this.setData({
       day: new Date().getDate(),
       month: new Date().getMonth() + 1
+    })
+  },
+
+  // 获取用户每日推荐数据
+  async getRecommendList() {
+    let recommendListData = await request('/recommend/songs')
+    // console.log(recommendListData);
+    // 更新状态数据
+    this.setData({
+      recommendList: recommendListData.recommend
     })
   },
 
